4233a6a32300a6044aa525c604b3065baca178a3
Josh Singer <josh.singer@glasgow.ac.uk>
2015-06-01 17:24:31 +0100

    db testing ideas.

---
+++ core/src/test/java/uk/ac/gla/cvr/gluetools/core/testdb/TestDB.java
@@ -0,0 +1,19 @@
+package uk.ac.gla.cvr.gluetools.core.testdb;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+public class TestDB {
+
+	@Test
+	public void test() throws Exception {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("glue-persistence-unit");
+		EntityManager entityManager = emf.createEntityManager();
+		entityManager.close();
+		emf.close();
+	}
+	
+}
