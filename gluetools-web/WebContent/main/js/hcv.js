70fc1bba4136ca3c54d1323a4454bb640c5ea1a3
Josh Singer <josh.singer@glasgow.ac.uk>
2015-07-24 11:50:45 +0100

    genotypes JSON loaded from a file. project publishable in Tomcat.

--- gluetools-web/WebContent/main/js/hcv.js
+++ gluetools-web/WebContent/main/js/hcv.js
@@ -1,29 +1,19 @@
 var hcvApp = angular.module('hcvApp', ['angularTreeview']);
 
-hcvApp.controller('searchCtrl', [ '$scope', function($scope) {
+hcvApp.controller('searchCtrl', [ '$scope', '$http', function($scope, $http) {
 		
-	$scope.treedata = 
-		[
-		    { "label" : "All HCV", "id" : "HCV", "children" : [
-		        { "label" : "Genotype 1", "id" : "genotype_1", "children" : 
-		        	[
-				        { "label" : "Subtype 1a", "id" : "genotype_1a", "children" : [] },
-				        { "label" : "Subtype 1b", "id" : "genotype_1b", "children" : [] },
-				        { "label" : "Subtype 1c", "id" : "genotype_1c", "children" : [] },
-		        	 ] },
-		        { "label" : "Genotype 2", "id" : "genotype_2", "children" : 
-		        	[
-				        { "label" : "Subtype 2a", "id" : "genotype_2a", "children" : [] },
-				        { "label" : "Subtype 2b", "id" : "genotype_2b", "children" : [] },
-		        	 ] },
-		    ]},
-		];   
+	$scope.genotypes = null
+    $http.get('../main/js/hcvGenotypes.json')
+        .success(function(data) {
+            $scope.genotypes = [ data ];
+        })
+        .error(function(data,status,error,config){
+            $scope.contents = [{heading:"Error",description:"Could not load json data"}];
+        });
 	
-	
-	
-	$scope.$watch( 'hcvGenotypes.currentNode', function( newObj, oldObj ) {
-	    if( $scope.hcvGenotypes && angular.isObject($scope.hcvGenotypes.currentNode) ) {
-	        $scope.genotypeID = $scope.hcvGenotypes.currentNode.id;
+	$scope.$watch( 'genotypesTree.currentNode', function( newObj, oldObj ) {
+	    if( $scope.genotypesTree && angular.isObject($scope.genotypesTree.currentNode) ) {
+	        $scope.genotypeID = $scope.genotypesTree.currentNode.id;
 	    }
 	}, false);
 
