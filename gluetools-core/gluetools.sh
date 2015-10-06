75e025b10cb1dd51996ed71236a4a68f30bd6168
Josh Singer <josh.singer@glasgow.ac.uk>
2015-10-06 18:17:06 +0100

    fixed versioning mess.

--- gluetools-core/gluetools.sh
+++ gluetools-core/gluetools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export GLUETOOLS_VERSION=`./getCurrentVersion.sh`
+export GLUETOOLS_VERSION=`(cd .. ; ./getCurrentVersion.sh)`
 gradle --quiet jarAll
 java -jar build/libs/gluetools-core-all-${GLUETOOLS_VERSION}.jar -c gluetools-config.xml "$@"
