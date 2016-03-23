6982b741f3a6b689b0e6747ac57e38fe8dada5a1
Josh Singer <josh.singer@glasgow.ac.uk>
2016-03-23 15:22:53 +0000

    DIGS importer no longer depends on the presence of Loci and LociLink tables.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/digs/importer/model/Loci.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.gla.cvr.gluetools.core.digs.importer.model;
-
-import uk.ac.gla.cvr.gluetools.core.digs.importer.model.auto._Loci;
-
-public class Loci extends _Loci {
-
-}
--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/digs/importer/model/LociLink.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.gla.cvr.gluetools.core.digs.importer.model;
-
-import uk.ac.gla.cvr.gluetools.core.digs.importer.model.auto._LociLink;
-
-public class LociLink extends _LociLink {
-
-}
