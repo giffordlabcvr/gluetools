5439622d40f9c8b24fcac08c326c1b14492df58b
Josh Singer <josh.singer@glasgow.ac.uk>
2016-05-30 18:52:43 +0100

    factored out frequency view part of dialog, and allowed toggle between "query clades" and "all clades".

--- gluetools-web/WebContent/www/hcvApp/controllers/displayVariationBase.js
+++ gluetools-web/WebContent/www/hcvApp/controllers/displayVariationBase.js
@@ -6,19 +6,38 @@ analysisTool.controller('displayVariationBase', ['$scope', '$modalInstance',
 		$scope.variationCategory = variationCategory;
 		$scope.variation = variation;
 		
+		$scope.data = {
+				freqView: "querySequenceClades",
+				queryCladeFreqs: [],
+				allCladeFreqs: []
+		};
+		
 		if($scope.variation.alignmentNote) {
-			$scope.publicSeqFreqs = [];
+			$scope.data.allCladeFreqs = $scope.variation.alignmentNote;
 			for(var i = 0; i < ancestorAlmtNames.length; i++) {
 				var freq = _.find($scope.variation.alignmentNote, function(almtNote) { 
 					return ancestorAlmtNames[i] == almtNote.alignmentName;
 				});
 				if(freq) {
-					$scope.publicSeqFreqs.push(freq);
+					$scope.data.queryCladeFreqs.push(freq);
 				}
 			}
 		}
 		
+		$scope.data.cladeFreqs = $scope.data.queryCladeFreqs;
 		
+		$scope.data.updateCladeFreqs = function() {
+			if($scope.data.freqView == "querySequenceClades") {
+				$scope.data.cladeFreqs = $scope.data.queryCladeFreqs;
+			} else {
+				$scope.data.cladeFreqs = $scope.data.allCladeFreqs;
+			}
+		}
+		
+		$scope.$watch( 'data.freqView', function(newObj, oldObj) {
+			$scope.data.updateCladeFreqs();
+		}, false);
+
 		addUtilsToScope($scope);
 		
 		$scope.close = function(){
