297d07ffd925f6ffd8df94af048218f67333d1b3
Josh Singer <josh.singer@glasgow.ac.uk>
2016-06-14 14:49:52 +0100

    build updates/fixes

--- gluetools-web/WebContent/www/gluetoolsApp/installation.js
+++ gluetools-web/WebContent/www/gluetoolsApp/installation.js
@@ -4,7 +4,7 @@ var installation = angular.module('installation', ['ui.bootstrap', 'hljs']);
 installation.controller('installationCtrl', 
 		[ '$scope', '$http', 
 		function($scope, $http) {
-		    $http.get('gluetools/downloads/gluetools-config.xml')
+		    $http.get('./downloads/gluetools-config.xml')
 	        .success(function(data) {
 	            $scope.gluetoolsXml = data;
 	        });
