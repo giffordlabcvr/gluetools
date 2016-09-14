40a04b5128d0718bdbe97316f0e5ad22111aa040
Josh Singer <josh.singer@glasgow.ac.uk>
2016-09-14 12:01:42 +0100

    updated templating.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/collation/exporting/fasta/AbstractFastaExporter.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/collation/exporting/fasta/AbstractFastaExporter.java
@@ -1,26 +1,24 @@
 package uk.ac.gla.cvr.gluetools.core.collation.exporting.fasta;
 
-import java.io.IOException;
-import java.io.StringWriter;
+import java.util.Optional;
 
 import org.w3c.dom.Element;
 
-import uk.ac.gla.cvr.gluetools.core.command.CommandException;
-import uk.ac.gla.cvr.gluetools.core.command.CommandException.Code;
 import uk.ac.gla.cvr.gluetools.core.datamodel.sequence.Sequence;
 import uk.ac.gla.cvr.gluetools.core.modules.ModulePlugin;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginConfigContext;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginUtils;
-import freemarker.template.SimpleScalar;
+import uk.ac.gla.cvr.gluetools.utils.FreemarkerUtils;
 import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateHashModel;
 import freemarker.template.TemplateModel;
 
 public class AbstractFastaExporter<T extends AbstractFastaExporter<T>> extends ModulePlugin<T> {
 
 	private static final String ID_TEMPLATE = "idTemplate";
 	private Template idTemplate;
+	
+	public static final String DEFAULT_ID_TEMPLATE = "${sequenceID}";
+
 	
 	public AbstractFastaExporter() {
 		super();
@@ -31,34 +29,16 @@ public class AbstractFastaExporter<T extends AbstractFastaExporter<T>> extends M
 	public void configure(PluginConfigContext pluginConfigContext,
 			Element configElem) {
 		super.configure(pluginConfigContext, configElem);
-		idTemplate = PluginUtils.configureFreemarkerTemplateProperty(pluginConfigContext, configElem, ID_TEMPLATE, false);
+		this.idTemplate = Optional.ofNullable(
+				PluginUtils.configureFreemarkerTemplateProperty(pluginConfigContext, configElem, ID_TEMPLATE, false))
+				.orElse(FreemarkerUtils.templateFromString(DEFAULT_ID_TEMPLATE, pluginConfigContext.getFreemarkerConfiguration()));
 	}
 
-	protected String generateFastaId(Sequence seq) {
-		String fastaId;
-		if(idTemplate == null) {
-			fastaId = seq.getSequenceID();
-		} else {
-			TemplateHashModel variableResolver = new TemplateHashModel() {
-				@Override
-				public TemplateModel get(String key) {
-					Object propValue = seq.readNestedProperty(key);
-					return propValue == null ? null : new SimpleScalar(propValue.toString()); 
-				}
-				@Override
-				public boolean isEmpty() { return false; }
-			};
-			StringWriter result = new StringWriter();
-			try {
-				idTemplate.process(variableResolver, result);
-			} catch (TemplateException e) {
-				throw new CommandException(e, Code.COMMAND_FAILED_ERROR, e.getLocalizedMessage());
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			fastaId = result.toString();
-		}
-		return fastaId;
+	protected String generateFastaId(Sequence sequence) {
+		TemplateModel templateModel = FreemarkerUtils.templateModelForGlueDataObject(sequence);
+		return FreemarkerUtils.processTemplate(idTemplate, templateModel);
 	}
+
+	
 
 }
