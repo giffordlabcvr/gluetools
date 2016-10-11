9eccab54dd695f554a4f71f1a21254b0a78007b6
Josh Singer <josh.singer@glasgow.ac.uk>
2016-10-11 16:52:56 +0100

    subclass may specify renderer module.

--- gluetools-web/WebContent/www/projectBrowser/controllers/renderableObjectBase.js
+++ gluetools-web/WebContent/www/projectBrowser/controllers/renderableObjectBase.js
@@ -1,12 +1,17 @@
-projectBrowser.controller('renderableObjectBaseCtrl', [ '$scope', 'glueObjectPath', 'glueWS', 'glueWebToolConfig', 'dialogs',
-        function($scope, glueObjectPath, glueWS, glueWebToolConfig, dialogs) {
+projectBrowser.controller('renderableObjectBaseCtrl', 
+		[ '$scope', 'glueObjectPath', 'rendererModule', 'glueWS', 'glueWebToolConfig', 'dialogs',
+        function($scope, glueObjectPath, rendererModule, glueWS, glueWebToolConfig, dialogs) {
 	
 	$scope.projectBrowserURL = glueWebToolConfig.getProjectBrowserURL();
 
 	console.log("renderableObjectBaseCtrl");
 	$scope.renderResult = null;
+	var cmdParams = {};
+	if(rendererModule != null) {
+		cmdParams.rendererModuleName = rendererModule
+	}
 	glueWS.runGlueCommand($scope.glueObjectPath, {
-    	"render-object": {} 
+    	"render-object": cmdParams
 	})
     .success(function(data, status, headers, config) {
 		  console.info('render result', data);
