0aec45806e3977aa5f4b98f2033f7a68043b02b0
Josh Singer <josh.singer@glasgow.ac.uk>
2016-11-02 14:01:45 +0000

    "create module" command auto generates module names as necessary.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/command/project/CreateModuleCommand.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/command/project/CreateModuleCommand.java
@@ -1,8 +1,10 @@
 package uk.ac.gla.cvr.gluetools.core.command.project;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 import org.w3c.dom.Document;
@@ -22,6 +24,7 @@ import uk.ac.gla.cvr.gluetools.core.command.result.CreateResult;
 import uk.ac.gla.cvr.gluetools.core.datamodel.GlueDataObject;
 import uk.ac.gla.cvr.gluetools.core.datamodel.module.Module;
 import uk.ac.gla.cvr.gluetools.core.datamodel.module.ModuleException;
+import uk.ac.gla.cvr.gluetools.core.logging.GlueLogger;
 import uk.ac.gla.cvr.gluetools.core.modules.ModulePluginFactory;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginConfigContext;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginFactory;
@@ -30,13 +33,17 @@ import uk.ac.gla.cvr.gluetools.utils.GlueXmlUtils;
 
 @CommandClass(
 		commandWords={"create", "module"}, 
-		docoptUsages={"(-t <moduleType> | -f <fileName> [-r] ) <moduleName>"},
+		docoptUsages={"(-t <moduleType> | -f <fileName> [-r] ) [<moduleName>]"},
 		description="Create a module",
 		docoptOptions={
 				"-t <moduleType>, --moduleType <moduleType>  Default config for a specific module type", 
 				"-f <fileName>, --fileName <fileName>        Config from an XML file",
 				"-r, --loadResources                         Also load dependent resources"},
-		furtherHelp="Creates a module of the specified type, with the default configuration for that module type.",
+		furtherHelp="Creates a module either of a specified type, with the default configuration for that type, "+
+				"or from an XML file, in which case the module type is specified by the document root element name."+
+				"\nIf no <moduleName> is specified, a name is auto-generated from the supplied <moduleType> or from the "+
+				"file part of <fileName> path, without the '.xml' extension. The module name is given a numeric suffix if "+
+				"necessary to ensure it is unique.",
 		metaTags={ CmdMeta.updatesDatabase, CmdMeta.consoleOnly } ) 
 	
 public class CreateModuleCommand extends Command<CreateResult> {
@@ -58,10 +65,16 @@ public class CreateModuleCommand extends Command<CreateResult> {
 		super.configure(pluginConfigContext, configElem);
 		this.moduleType = PluginUtils.configureStringProperty(configElem, MODULE_TYPE, false);
 		this.fileName = PluginUtils.configureStringProperty(configElem, FILE_NAME, false);
-		this.moduleName = PluginUtils.configureStringProperty(configElem, MODULE_NAME, true);
+		this.moduleName = PluginUtils.configureStringProperty(configElem, MODULE_NAME, false);
 		this.loadResources = Optional.ofNullable(PluginUtils.configureBooleanProperty(configElem, LOAD_RESOURCES, false)).orElse(false);
 		if(moduleType == null && fileName == null) {
 			throw new CommandException(Code.COMMAND_USAGE_ERROR, "Either <moduleType> or <fileName> must be specified.");
+		}
+		if(moduleType != null && fileName != null) {
+			throw new CommandException(Code.COMMAND_USAGE_ERROR, "Only one of <moduleType> or <fileName> may be specified.");
+		}
+		if(fileName != null && !fileName.endsWith(".xml")) {
+			throw new CommandException(Code.COMMAND_USAGE_ERROR, "The <fileName> must end with .xml");
 		}
 		if(fileName == null && loadResources) {
 			throw new CommandException(Code.COMMAND_USAGE_ERROR, "The --loadResources option may only be used if <fileName> is specified.");
@@ -73,7 +86,33 @@ public class CreateModuleCommand extends Command<CreateResult> {
 		
 		byte[] config = null;
 		
-		Module module = GlueDataObject.create(cmdContext, Module.class, Module.pkMap(moduleName), false);
+		String nameToUse = moduleName;
+		if(nameToUse == null) {
+			String namePrefix;
+			if(moduleType != null) {
+				namePrefix = moduleType;
+			} else {
+				File file = new File(fileName);
+				String filePart = file.getName();
+				namePrefix = filePart.substring(0, filePart.length()-4); // remove ".xml"
+			}
+			String nameToTry = null;
+			int suffix = 1;
+			while(true) {
+				if(nameToTry == null) {
+					nameToTry = namePrefix;
+				} else {
+					nameToTry = namePrefix+Integer.toString(suffix);
+					suffix++;
+				}
+				Module module = GlueDataObject.lookup(cmdContext, Module.class, Module.pkMap(nameToTry), true);
+				if(module == null) {
+					nameToUse = nameToTry;
+					break;
+				}
+			}
+		}
+		Module module = GlueDataObject.create(cmdContext, Module.class, Module.pkMap(nameToUse), false);
 		if(moduleType != null) {
 			ModulePluginFactory pluginFactory = PluginFactory.get(ModulePluginFactory.creator);
 			if(!pluginFactory.getElementNames().contains(moduleType)) {
@@ -90,6 +129,7 @@ public class CreateModuleCommand extends Command<CreateResult> {
 			module.loadConfig(consoleCmdContext, fileName, loadResources);
 		}
 		cmdContext.commit();
+		GlueLogger.log(Level.FINEST, "Created new module '"+nameToUse+"' of type "+module.getType());
 		return new CreateResult(Module.class, 1);
 	}
 
