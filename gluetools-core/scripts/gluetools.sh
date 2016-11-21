6dd2033f9e14fb9bff68c3168f13a59a5b96550e
Josh Singer <josh.singer@glasgow.ac.uk>
2016-11-21 15:19:58 +0000

    gluetools.sh script uses local glue configs if -l option is used. separate build tasks to deploy glue configs for josh and rob on alpha.

--- gluetools-core/scripts/gluetools.sh
+++ gluetools-core/scripts/gluetools.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+
+options=':l'
+localConfig='false'
+while getopts $options option
+do
+    case $option in
+        l  )    localConfig='true';;
+        \? )    if (( (err & ERROPTS) != ERROPTS ))
+                then
+                    error $NOEXIT $ERROPTS "Unknown option."
+                fi;;
+        *  )    echo "Missing option argument."
+		exit 1;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 export GLUETOOLS_VERSION=`(cd ${GLUETOOLS_HOME}/.. ; ./getCurrentVersion.sh)`
+export GLUETOOLS_CONFIG_XML=${GLUETOOLS_HOME}/gluetools-config.xml
+if [ $localConfig == "true" ]
+then
+if [ -e "local-gluetools-config.xml" ]
+then
+    echo "Using config local-gluetools-config.xml"
+    export GLUETOOLS_CONFIG_XML="local-gluetools-config.xml"
+else
+    echo "local-gluetools-config.xml does not exist"
+    exit 1 
+fi
+fi
 (cd ${GLUETOOLS_HOME} ; gradle --quiet jarAll)
-java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar ${GLUETOOLS_HOME}/build/libs/gluetools-core-all-${GLUETOOLS_VERSION}.jar -c ${GLUETOOLS_HOME}/gluetools-config.xml "$@"
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar ${GLUETOOLS_HOME}/build/libs/gluetools-core-all-${GLUETOOLS_VERSION}.jar -c ${GLUETOOLS_CONFIG_XML} "$@"
