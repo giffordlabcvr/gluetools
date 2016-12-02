7357c09b378fb61ecb02e1b3e3d90705e165929e
Josh Singer <josh.singer@glasgow.ac.uk>
2016-12-02 17:30:09 +0000

    more genotypes work.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/genotyping/maxlikelihood/GenotypeCategoryResult.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/genotyping/maxlikelihood/GenotypeCategoryResult.java
@@ -1,5 +1,11 @@
 package uk.ac.gla.cvr.gluetools.core.genotyping.maxlikelihood;
 
+import java.util.List;
+
 public class GenotypeCategoryResult {
 
+	private String categoryName;
+	
+	private List<GenotypeAlignmentResult> alignmentResults;
+	
 }
