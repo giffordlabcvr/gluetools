cb0a5e5349527e118db580ef4613bb4385194bf9
Josh Singer <josh.singer@glasgow.ac.uk>
2017-03-13 11:29:25 +0000

    Allow iso country text file populator column to have value converters etc.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/collation/populating/textfile/IsoCountryTextFilePopulatorColumn.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/collation/populating/textfile/IsoCountryTextFilePopulatorColumn.java
@@ -1,9 +1,13 @@
 package uk.ac.gla.cvr.gluetools.core.collation.populating.textfile;
 
+import java.util.List;
+
 import org.w3c.dom.Element;
 
+import uk.ac.gla.cvr.gluetools.core.collation.populating.regex.RegexExtractorFormatter;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginClass;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginConfigContext;
+import uk.ac.gla.cvr.gluetools.core.plugins.PluginFactory;
 import uk.ac.gla.cvr.gluetools.core.plugins.PluginUtils;
 import uk.ac.gla.cvr.gluetools.utils.IsoCountryUtils;
 import uk.ac.gla.cvr.gluetools.utils.IsoCountryUtils.CodeStyle;
@@ -17,8 +21,11 @@ public class IsoCountryTextFilePopulatorColumn extends BaseTextFilePopulatorColu
 	public void configure(PluginConfigContext pluginConfigContext, Element configElem)  {
 		super.configure(pluginConfigContext, configElem);
 		this.codeStyle = PluginUtils.configureEnum(CodeStyle.class, configElem, "@codeStyle", true);
-		setValueConverters(IsoCountryUtils.isoCountryValueConverters(codeStyle));
-		setMainExtractor(null);
+		List<RegexExtractorFormatter> valueConverters = PluginFactory.createPlugins(pluginConfigContext, RegexExtractorFormatter.class, 
+				PluginUtils.findConfigElements(configElem, "valueConverter"));
+		valueConverters.addAll(IsoCountryUtils.isoCountryValueConverters(codeStyle));
+		setValueConverters(valueConverters);
+		setMainExtractor(PluginFactory.createPlugin(pluginConfigContext, RegexExtractorFormatter.class, configElem));
 	}
 
 }
