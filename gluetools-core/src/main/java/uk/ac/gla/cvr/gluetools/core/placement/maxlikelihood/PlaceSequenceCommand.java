1d0b3f37655a5a128aa1c1f7a0c2d0e602db4017
Josh Singer <josh.singer@glasgow.ac.uk>
2017-07-11 16:32:33 +0100

    place sequence command altered to using cayenne paging mechanism.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/placement/maxlikelihood/PlaceSequenceCommand.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/placement/maxlikelihood/PlaceSequenceCommand.java
@@ -37,30 +37,33 @@ import uk.ac.gla.cvr.gluetools.utils.GlueXmlUtils;
 @CommandClass(
 		commandWords={"place", "sequence"}, 
 		description = "Place one or more stored sequences into a phylogeny", 
-		docoptUsages = { "(-w <whereClause> | -a) [-b <batchSize>] [-d <dataDir>] -o <outputFilePrefix>" },
+		docoptUsages = { "(-w <whereClause> | -a) [-p <pageSize>] [-l <fetchLimit>] [-f <fetchOffset>] [-d <dataDir>] -o <outputFile>" },
 		docoptOptions = { 
 				"-w <whereClause>, --whereClause <whereClause>  Qualify the sequences to be placed",
 				"-a, --allSequences                             Place all sequences in the project",
-				"-b <batchSize>, --batchSize <batchSize>        Batch size",
+				"-p <pageSize>, --pageSize <pageSize>           Tune ORM page size",
+				"-l <fetchLimit>, --fetchLimit <fetchLimit>     Limit max number of records",
+				"-f <fetchOffset>, --fetchOffset <fetchOffset>  Record number offset",
 				"-o <outputFile>, --outputFile <outputFile>     Output file path for placement results",
 				"-d <dataDir>, --dataDir <dataDir>              Save algorithmic data in this directory",
 		},
-		furtherHelp = "Where the operation happens in multiple batches, separate algorithmic data directories "
-			+"will be used for each batch.",
+		furtherHelp = "",
 		metaTags = {CmdMeta.consoleOnly}	
 )
 public class PlaceSequenceCommand extends AbstractPlaceCommand<OkResult> {
 
 	public final static String WHERE_CLAUSE = "whereClause";
 	public final static String ALL_SEQUENCES = "allSequences";
-	public static final String BATCH_SIZE = "batchSize";
+	public static final String PAGE_SIZE = "pageSize";
 	public static final String FETCH_LIMIT = "fetchLimit";
 	public static final String FETCH_OFFSET = "fetchOffset";
 
 	
 	private Expression whereClause;
 	private Boolean allSequences;
-	private int batchSize;
+	private Optional<Integer> fetchLimit;
+	private Optional<Integer> fetchOffset;
+	private int pageSize;
 	
 	@Override
 	public void configure(PluginConfigContext pluginConfigContext, Element configElem) {
@@ -70,7 +73,10 @@ public class PlaceSequenceCommand extends AbstractPlaceCommand<OkResult> {
 		if(this.whereClause == null && this.allSequences == null) {
 			throw new CommandException(Code.COMMAND_USAGE_ERROR, "Either <whereClause> or --allSequences must be specified");
 		}
-		batchSize = Optional.ofNullable(PluginUtils.configureIntProperty(configElem, BATCH_SIZE, false)).orElse(250);
+		pageSize = Optional.ofNullable(PluginUtils.configureIntProperty(configElem, PAGE_SIZE, false)).orElse(250);
+		fetchLimit = Optional.ofNullable(PluginUtils.configureIntProperty(configElem, FETCH_LIMIT, false));
+		fetchOffset = Optional.ofNullable(PluginUtils.configureIntProperty(configElem, FETCH_OFFSET, false));
+
 
 	}
 
@@ -84,45 +90,26 @@ public class PlaceSequenceCommand extends AbstractPlaceCommand<OkResult> {
 		} else {
 			selectQuery = new SelectQuery(Sequence.class, this.whereClause);
 		}
-		int numSequences = GlueDataObject.count(cmdContext, selectQuery);
-		selectQuery.setPageSize(batchSize);
-		selectQuery.setFetchLimit(batchSize);
-		int offset = 0;
-		PlacerResultInternal fullResult = null;
-		while(offset < numSequences) {
-			selectQuery.setFetchOffset(offset);
-			int firstBatchIndex = offset+1;
-			int lastBatchIndex = Math.min(offset+batchSize, numSequences);
-			GlueLogger.getGlueLogger().log(Level.FINEST, "Retrieving sequences "+firstBatchIndex+" to "+lastBatchIndex+" of "+numSequences);
-			List<Sequence> currentBatch = GlueDataObject.query(cmdContext, Sequence.class, selectQuery);
-			GlueLogger.getGlueLogger().log(Level.FINEST, "Processing sequences "+firstBatchIndex+" to "+lastBatchIndex+" of "+numSequences);
-			Map<String, DNASequence> querySequenceMap = new LinkedHashMap<String, DNASequence>();
-			currentBatch.forEach(seq -> {
-				querySequenceMap.put(seq.getSource().getName()+"/"+seq.getSequenceID(), 
-						FastaUtils.ntStringToSequence(seq.getSequenceObject().getNucleotides(cmdContext)));
-			});
-			String requestedDataDir = getDataDir();
-			File dataDirFile = null;
-			if(batchSize >= numSequences) {
-				dataDirFile = CommandUtils.ensureDataDir(consoleCmdContext, requestedDataDir);
-			} else {
-				dataDirFile = CommandUtils.ensureDataDir(consoleCmdContext, requestedDataDir+"_"+firstBatchIndex+"_"+lastBatchIndex);
-			}
-			PlacerResultInternal batchResult = maxLikelihoodPlacer.place(consoleCmdContext, querySequenceMap, dataDirFile);
-			if(fullResult == null) {
-				fullResult = batchResult;
-			} else {
-				fullResult.getQueryResults().putAll(batchResult.getQueryResults());
-			}
-			offset = offset+batchSize;
-			cmdContext.newObjectContext();
-		}
-		GlueLogger.getGlueLogger().log(Level.FINEST, "Processed "+numSequences+" sequences ");
-		CommandDocument placerResultCmdDocument = PojoDocumentUtils.pojoToCommandDocument(fullResult.toPojoResult());
+		selectQuery.setPageSize(pageSize);
+		fetchLimit.ifPresent(limit -> selectQuery.setFetchLimit(limit));
+		fetchOffset.ifPresent(offset -> selectQuery.setFetchOffset(offset));
+		GlueLogger.getGlueLogger().log(Level.FINEST, "Retrieving sequences");
+		List<Sequence> sequences = GlueDataObject.query(cmdContext, Sequence.class, selectQuery);
+		GlueLogger.getGlueLogger().log(Level.FINEST, "Retrieved "+sequences.size()+" sequences, processing...");
+		Map<String, DNASequence> querySequenceMap = new LinkedHashMap<String, DNASequence>();
+		sequences.forEach(seq -> {
+			querySequenceMap.put(seq.getSource().getName()+"/"+seq.getSequenceID(), 
+					FastaUtils.ntStringToSequence(seq.getSequenceObject().getNucleotides(cmdContext)));
+		});
+		String requestedDataDir = getDataDir();
+		File dataDirFile = CommandUtils.ensureDataDir(consoleCmdContext, requestedDataDir);
+		PlacerResultInternal placerResultInternal = maxLikelihoodPlacer.place(consoleCmdContext, querySequenceMap, dataDirFile);
+		CommandDocument placerResultCmdDocument = PojoDocumentUtils.pojoToCommandDocument(placerResultInternal.toPojoResult());
 		Document placerResultXmlDoc = CommandDocumentXmlUtils.commandDocumentToXmlDocument(placerResultCmdDocument);
 		byte[] placerResultXmlBytes = GlueXmlUtils.prettyPrint(placerResultXmlDoc);
-		consoleCmdContext.saveBytes(getOutputFile(), placerResultXmlBytes);
-		GlueLogger.getGlueLogger().log(Level.FINEST, "Processed "+numSequences+" sequences ");
+		String outputFile = getOutputFile();
+		consoleCmdContext.saveBytes(outputFile, placerResultXmlBytes);
+		GlueLogger.getGlueLogger().log(Level.FINEST, "Saved placerResult to "+outputFile);
 		return new OkResult();
 	}
 
