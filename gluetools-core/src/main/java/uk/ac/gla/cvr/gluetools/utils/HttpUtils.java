42bf67c1bbaa5f30041a564d274a7d64507de430
Josh Singer <josh.singer@glasgow.ac.uk>
2017-08-03 12:41:04 +0100

    changed proxy property names to include https (used for NCBI importer) proxy settings now use URL with scheme.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/utils/HttpUtils.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/utils/HttpUtils.java
@@ -2,6 +2,7 @@ package uk.ac.gla.cvr.gluetools.utils;
 
 public class HttpUtils {
 	public static final String HTTP_PROXY_ENABLED = "gluetools.core.http.proxy.enabled";
-	public static final String HTTP_PROXY_HOST = "gluetools.core.http.proxy.host";
-	public static final String HTTP_PROXY_PORT = "gluetools.core.http.proxy.port";
+	public static final String HTTP_PROXY_URL = "gluetools.core.http.proxy.url";
+	public static final String HTTPS_PROXY_ENABLED = "gluetools.core.https.proxy.enabled";
+	public static final String HTTPS_PROXY_URL = "gluetools.core.https.proxy.url";
 }
