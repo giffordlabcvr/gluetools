19b723be72979ac2aa4846d28aa621cf66e9402c
Josh Singer <josh.singer@glasgow.ac.uk>
2017-12-14 14:37:00 +0000

    10 full-length sequences from genbank loaded at the end of the build, metadata is extracted, each sequence is genotyped and then added into the alignment tree.

---
+++ gluetools-core/exampleProject/glue/addExampleSeqsToTree.js
@@ -0,0 +1,33 @@
+
+
+function addToAlignment(sourceName, sequenceID, almtName) {
+	glue.inMode("alignment/"+almtName, function() {
+		glue.command(["add", "member", sourceName, sequenceID]);
+	});
+	glue.command(["compute", "alignment", almtName, "hevCompoundAligner", 
+	              "--whereClause", "sequence.source.name = '"+sourceName+"' and sequence.sequenceID = '"+sequenceID+"'"]);
+}
+
+var genotypingResults;
+
+glue.inMode("module/hevMaxLikelihoodGenotyper", function() {
+	genotypingResults = glue.command(["genotype", "sequence", "--whereClause", "source.name = 'ncbi-hev-examples'"], {convertTableToObjects:true});
+});
+
+for(var i = 0; i < genotypingResults.length; i++) {
+	var result = genotypingResults[i];
+	var queryNameBits = result.queryName.split("/");
+	var sourceName = queryNameBits[0];
+	var sequenceID = queryNameBits[1];
+	
+	var genotypeAlmt = result.genotypeFinalClade;
+	var subtypeAlmt = result.subtypeFinalClade;
+	if(genotypeAlmt != null) {
+		if(subtypeAlmt != null) {
+			addToAlignment(sourceName, sequenceID, subtypeAlmt);
+		} else {
+			addToAlignment(sourceName, sequenceID, genotypeAlmt);
+		}
+	}
+	
+}
