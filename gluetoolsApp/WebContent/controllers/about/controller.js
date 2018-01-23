b631a9c19d91ae22e4735dd2cc7c8ea9ee61ddc8
Josh Singer <josh.singer@glasgow.ac.uk>
2018-01-23 15:29:17 +0000

    bunch of documentation on the module system.

--- gluetoolsApp/WebContent/controllers/about/controller.js
+++ gluetoolsApp/WebContent/controllers/about/controller.js
@@ -1,4 +1,12 @@
 gluetoolsApp.controller('aboutCtrl', 
-		[ '$scope', 
-		function($scope) {
+		[ '$scope', '$http', '$location', '$anchorScroll',
+			function($scope, $http, $location, $anchorScroll) {
+
+			$scope.scrollTo = function(id) {
+				var old = $location.hash();
+				$location.hash(id);
+				$anchorScroll();
+				$location.hash(old);
+			};
+
 		} ]);
--- gluetoolsApp/WebContent/controllers/userGuide/modules/controller.js
+++ gluetoolsApp/WebContent/controllers/userGuide/modules/controller.js
@@ -1,5 +1,18 @@
 gluetoolsApp.controller('modulesCtrl', 
-		[ '$scope', '$http', 
-		function($scope, $http) {
-			
+		[ '$scope', '$http', '$location', '$anchorScroll',
+			function($scope, $http, $location, $anchorScroll) {
+
+			$scope.scrollTo = function(id) {
+				var old = $location.hash();
+				$location.hash(id);
+				$anchorScroll();
+				$location.hash(old);
+			};
+
+		    $http.get('./controllers/userGuide/modules/raxmlPhylogenyGenerator.xml')
+	        .success(function(data) {
+	            $scope.raxmlPhylogenyGenerator = data;
+	            console.log("raxmlPhylogenyGenerator", $scope.raxmlPhylogenyGenerator);
+	        });
+
 		} ]);
