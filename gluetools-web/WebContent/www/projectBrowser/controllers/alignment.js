c06acc5886e696bf8fa454df4ae691aa21c45f20
Josh Singer <josh.singer@glasgow.ac.uk>
2018-06-04 17:22:45 +0100

    infrastructure for region / development status filters where later "m49" naming is used.

--- gluetools-web/WebContent/www/projectBrowser/controllers/alignment.js
+++ gluetools-web/WebContent/www/projectBrowser/controllers/alignment.js
@@ -470,5 +470,160 @@ projectBrowser.controller('alignmentCtrl',
 				                        				 },
 				                        			];
 			};
+
+			
+			$scope.globalRegionFilterM49 = function() {
+				// note property here is a dummy value.
+                return { property:"globalRegion", nullProperty:"sequence.m49_country", displayName: "Global Region", filterHints: 
+	            	{ type: "StringFromFixedValueSet",
+                	  generateCustomDefault: function() {
+	            		  return {
+	            			  fixedValue: $scope.globalRegionFixedValueSetM49[0]
+	            		  };
+	            	  },
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
+                		  var fakeFilterElem = {
+                		    property:custom.fixedValue.property,
+                		    nullProperty:filterElem.nullProperty,
+                			type: "String",
+                			predicate: {
+                				operator: filterElem.predicate.operator,
+                				operand: [custom.fixedValue.value]
+                			}
+                		  };
+                		  var cayennePredicate = filterUtils.filterElemToCayennePredicate(fakeFilterElem);
+                		  // we want notmatches here to allow sequences with countries that have a null region/subregion/intregion.
+                		  if(filterElem.predicate.operator == 'notmatches') {
+                			  cayennePredicate = "( ( "+custom.fixedValue.property + " = null ) or ( " + cayennePredicate + " ) )";
+                		  }
+                		  return cayennePredicate;
+                  	  },
+	            	  generateFixedValueSet: function() {
+	            		  return $scope.globalRegionFixedValueSetM49;
+	            	  }
+	            	}
+                };
+			};
+			$scope.initGlobalRegionFixedValueSetM49 = function () {
+				$scope.globalRegionFixedValueSetM49 = [];
+
+				glueWS.runGlueCommand("", {
+				    "multi-render":{
+				        "tableName":"m49_region",
+				        "allObjects":"true",
+				        "rendererModuleName":"m49RegionTreeRenderer"
+				    }
+				})
+				.success(function(data, status, headers, config) {
+					var multiRenderResult = data.multiRenderResult;
+					console.info('m49 region multi-render result', data.multiRenderResult);
+					$scope.globalRegionFixedValueSetM49 = [];
+					for(var i = 0; i < multiRenderResult.resultDocument.length; i++) {
+						var m49Region = multiRenderResult.resultDocument[i].m49Region;
+						$scope.globalRegionFixedValueSetM49.push({
+							property:"sequence.m49_country.m49_region",
+				   			  value:m49Region.id,
+				   			  indent:0,
+				   			  displayName:m49Region.displayName
+						});
+						if(m49Region.m49SubRegion != null) {
+							for(var j = 0; j < m49Region.m49SubRegion.length; j++) {
+								var m49SubRegion = m49Region.m49SubRegion[j];
+								$scope.globalRegionFixedValueSetM49.push({
+									property:"sequence.m49_country.m49_sub_region",
+						   			  value:m49SubRegion.id,
+						   			  indent:1,
+						   			  displayName:m49SubRegion.displayName
+								});
+								if(m49SubRegion.m49IntermediateRegion != null) {
+									for(var k = 0; k < m49SubRegion.m49IntermediateRegion.length; k++) {
+										var m49IntermediateRegion = m49SubRegion.m49IntermediateRegion[k];
+										$scope.globalRegionFixedValueSetM49.push({
+											property:"sequence.m49_country.m49_intermediate_region",
+								   			  value:m49IntermediateRegion.id,
+								   			  indent:2,
+								   			  displayName:m49IntermediateRegion.displayName
+										});
+									}
+								}
+							}
+						}
+					}
+					console.info('$scope.globalRegionFixedValueSet', $scope.globalRegionFixedValueSetM49);
+				})
+				.error(glueWS.raiseErrorDialog(dialogs, "retrieving M49 region tree"));
+			};
+			
+			$scope.developmentStatusFilterM49 = function() {
+                // note property here is a dummy value.
+                return { property:"developmentStatus", nullProperty:"sequence.m49_country", displayName: "Country Development Status", filterHints: 
+	            	{ type: "StringFromFixedValueSet",
+                	  generateCustomDefault: function() {
+	            		  return {
+	            			  fixedValue: $scope.developmentStatusFixedValueSetM49[0]
+	            		  };
+	            	  },
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
+                		  var type;
+                		  if(custom.fixedValue.property == 'sequence.m49_country.development_status') {
+                			  type = "String";
+                		  } else {
+                			  type = "Boolean";
+                		  }
+                		  var fakeFilterElem = {
+                		    property:custom.fixedValue.property,
+                		    nullProperty:filterElem.nullProperty,
+                			type: filterElem.type,
+                			predicate: {
+                				operator: filterElem.predicate.operator,
+                				operand: [custom.fixedValue.value]
+                			}
+                		  };
+                		  return filterUtils.filterElemToCayennePredicate(fakeFilterElem);
+                  	  },
+	            	  generateFixedValueSet: function() {
+	            		  return $scope.developmentStatusFixedValueSetM49;
+	            	  }
+	            	}
+                };
+			};
+			$scope.initDevelopmentStatusFixedValueSetM49 = function () {
+				$scope.developmentStatusFixedValueSetM49 = [
+				                        				 {
+				                        					 property:"sequence.m49_country.development_status",
+				                        					 value:"developed",
+				                        					 indent:0,
+				                        					 displayName:"Developed country"
+				                        				 },
+				                        				 {
+				                        					 property:"sequence.m49_country.development_status",
+				                        					 value:"developing",
+				                        					 indent:0,
+				                        					 displayName:"Developing country"
+				                        				 },
+				                        				 {
+				                        					 property:"sequence.m49_country.is_ldc",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Least developed country (LDC)"
+				                        				 },
+				                        				 {
+				                        					 property:"sequence.m49_country.is_lldc",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Landlocked developing country (LLDC)"
+				                        				 },
+				                        				 {
+				                        					 property:"sequence.m49_country.is_sids",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Small island developing state (SIDS)"
+				                        				 },
+				                        			];
+			};
+
+			
 			
 }]);
--- gluetools-web/WebContent/www/projectBrowser/controllers/sequences.js
+++ gluetools-web/WebContent/www/projectBrowser/controllers/sequences.js
@@ -316,7 +316,159 @@ projectBrowser.controller('sequencesCtrl',
 			};
 
 			
+			$scope.globalRegionFilterM49 = function() {
+				// note property here is a dummy value.
+                return { property:"globalRegion", nullProperty:"m49_country", displayName: "Global Region", filterHints: 
+	            	{ type: "StringFromFixedValueSet",
+                	  generateCustomDefault: function() {
+	            		  return {
+	            			  fixedValue: $scope.globalRegionFixedValueSetM49[0]
+	            		  };
+	            	  },
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
+                		  var fakeFilterElem = {
+                		    property:custom.fixedValue.property,
+                		    nullProperty:filterElem.nullProperty,
+                			type: "String",
+                			predicate: {
+                				operator: filterElem.predicate.operator,
+                				operand: [custom.fixedValue.value]
+                			}
+                		  };
+                		  var cayennePredicate = filterUtils.filterElemToCayennePredicate(fakeFilterElem);
+                		  // we want notmatches here to allow sequences with countries that have a null region/subregion/intregion.
+                		  if(filterElem.predicate.operator == 'notmatches') {
+                			  cayennePredicate = "( ( "+custom.fixedValue.property + " = null ) or ( " + cayennePredicate + " ) )";
+                		  }
+                		  return cayennePredicate;
+                  	  },
+	            	  generateFixedValueSet: function() {
+	            		  return $scope.globalRegionFixedValueSetM49;
+	            	  }
+	            	}
+                };
+			};
+			$scope.initGlobalRegionFixedValueSetM49 = function () {
+				$scope.globalRegionFixedValueSetM49 = [];
+
+				glueWS.runGlueCommand("", {
+				    "multi-render":{
+				        "tableName":"m49_region",
+				        "allObjects":"true",
+				        "rendererModuleName":"m49RegionTreeRenderer"
+				    }
+				})
+				.success(function(data, status, headers, config) {
+					var multiRenderResult = data.multiRenderResult;
+					console.info('m49 region multi-render result', data.multiRenderResult);
+					$scope.globalRegionFixedValueSetM49 = [];
+					for(var i = 0; i < multiRenderResult.resultDocument.length; i++) {
+						var m49Region = multiRenderResult.resultDocument[i].m49Region;
+						$scope.globalRegionFixedValueSetM49.push({
+							property:"m49_country.m49_region",
+				   			  value:m49Region.id,
+				   			  indent:0,
+				   			  displayName:m49Region.displayName
+						});
+						if(m49Region.m49SubRegion != null) {
+							for(var j = 0; j < m49Region.m49SubRegion.length; j++) {
+								var m49SubRegion = m49Region.m49SubRegion[j];
+								$scope.globalRegionFixedValueSetM49.push({
+									property:"m49_country.m49_sub_region",
+						   			  value:m49SubRegion.id,
+						   			  indent:1,
+						   			  displayName:m49SubRegion.displayName
+								});
+								if(m49SubRegion.m49IntermediateRegion != null) {
+									for(var k = 0; k < m49SubRegion.m49IntermediateRegion.length; k++) {
+										var m49IntermediateRegion = m49SubRegion.m49IntermediateRegion[k];
+										$scope.globalRegionFixedValueSetM49.push({
+											property:"m49_country.m49_intermediate_region",
+								   			  value:m49IntermediateRegion.id,
+								   			  indent:2,
+								   			  displayName:m49IntermediateRegion.displayName
+										});
+									}
+								}
+							}
+						}
+					}
+					console.info('$scope.globalRegionFixedValueSetM49', $scope.globalRegionFixedValueSetM49);
+				})
+				.error(glueWS.raiseErrorDialog(dialogs, "retrieving M49 region tree"));
+			};
 			
+			$scope.developmentStatusFilterM49 = function() {
+                // note property here is a dummy value.
+                return { property:"developmentStatus", nullProperty:"m49_country", displayName: "Country Development Status", filterHints: 
+	            	{ type: "StringFromFixedValueSet",
+                	  generateCustomDefault: function() {
+	            		  return {
+	            			  fixedValue: $scope.developmentStatusFixedValueSetM49[0]
+	            		  };
+	            	  },
+                	  generatePredicateFromCustom: function(filterElem) {
+                		  var custom = filterElem.custom;
+                		  var type;
+                		  if(custom.fixedValue.property == 'm49_country.development_status') {
+                			  type = "String";
+                		  } else {
+                			  type = "Boolean";
+                		  }
+                		  var fakeFilterElem = {
+                		    property:custom.fixedValue.property,
+                		    nullProperty:filterElem.nullProperty,
+                			type: filterElem.type,
+                			predicate: {
+                				operator: filterElem.predicate.operator,
+                				operand: [custom.fixedValue.value]
+                			}
+                		  };
+                		  return filterUtils.filterElemToCayennePredicate(fakeFilterElem);
+                  	  },
+	            	  generateFixedValueSet: function() {
+	            		  return $scope.developmentStatusFixedValueSetM49;
+	            	  }
+	            	}
+                };
+			};
+			$scope.initDevelopmentStatusFixedValueSetM49 = function () {
+				$scope.developmentStatusFixedValueSetM49 = [
+				                        				 {
+				                        					 property:"m49_country.development_status",
+				                        					 value:"developed",
+				                        					 indent:0,
+				                        					 displayName:"Developed country"
+				                        				 },
+				                        				 {
+				                        					 property:"m49_country.development_status",
+				                        					 value:"developing",
+				                        					 indent:0,
+				                        					 displayName:"Developing country"
+				                        				 },
+				                        				 {
+				                        					 property:"m49_country.is_ldc",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Least developed country (LDC)"
+				                        				 },
+				                        				 {
+				                        					 property:"m49_country.is_lldc",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Landlocked developing country (LLDC)"
+				                        				 },
+				                        				 {
+				                        					 property:"m49_country.is_sids",
+				                        					 value:"true",
+				                        					 indent:1,
+				                        					 displayName:"Small island developing state (SIDS)"
+				                        				 },
+				                        			];
+			};
+
+	
 			
 
 }]);
