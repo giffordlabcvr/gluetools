b8bf46bb267ac7e16576c95585f1a1ed7ba6c68d
Josh Singer <josh.singer@glasgow.ac.uk>
2018-09-04 09:51:34 +0100

    bugfix for codonAwareBlastAligner to deal with the situation where the feature location is not defined on the constrainingReference but also allowFeatureToBeMissing has not be set to true. Previously a null pointer exception was thrown, now a more informative error is given.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/curation/aligners/codonAwareBlast/CodonAwareBlastAligner.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/curation/aligners/codonAwareBlast/CodonAwareBlastAligner.java
@@ -45,6 +45,7 @@ import uk.ac.gla.cvr.gluetools.core.command.CommandContext;
 import uk.ac.gla.cvr.gluetools.core.command.project.referenceSequence.featureLoc.FeatureLocAminoAcidCommand;
 import uk.ac.gla.cvr.gluetools.core.curation.aligners.Aligner;
 import uk.ac.gla.cvr.gluetools.core.curation.aligners.AlignerException;
+import uk.ac.gla.cvr.gluetools.core.curation.aligners.AlignerException.Code;
 import uk.ac.gla.cvr.gluetools.core.curation.aligners.SupportsComputeConstrained;
 import uk.ac.gla.cvr.gluetools.core.curation.aligners.blast.AbstractBlastAligner;
 import uk.ac.gla.cvr.gluetools.core.curation.aligners.codonAwareBlast.CodonAwareBlastAligner.CodonAwareBlastAlignerResult;
@@ -98,8 +99,12 @@ public class CodonAwareBlastAligner extends AbstractBlastAligner<CodonAwareBlast
 		String featureName = getFeatureName();
 		FeatureLocation featureLoc = GlueDataObject.lookup(cmdContext, FeatureLocation.class, FeatureLocation.pkMap(refName, featureName), true);
 		
-		if(featureLoc == null && allowFeatureToBeMissing) {
-			return new CodonAwareBlastAlignerResult(Collections.emptyMap());
+		if(featureLoc == null) {
+			if(allowFeatureToBeMissing) {
+				return new CodonAwareBlastAlignerResult(Collections.emptyMap());
+			} else {
+				throw new AlignerException(Code.MISSING_FEATURE_LOCATION, refName, featureName);
+			}
 		}
 		
 		List<LabeledAminoAcid> featureLocAminoAcids = FeatureLocAminoAcidCommand.featureLocAminoAcids(cmdContext, featureLoc);
