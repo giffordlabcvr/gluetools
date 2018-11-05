8baa2563fc611585c615999fe7b3184f4b4eb5b0
Josh Singer <josh.singer@glasgow.ac.uk>
2018-11-05 16:51:48 +0000

    docker build split over two containers.

--- gluetools-core/docker/initMySQL.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-/opt/gluetools/bin/startMySQL.sh
-echo 'GRANT ALL ON GLUE_TOOLS.* TO gluetools@localhost identified by "glue12345";' | mysql -u root --password=root123
-echo 'create database GLUE_TOOLS character set UTF8;' | mysql -u gluetools --password=glue12345 
---
+++ gluetools-mysql-docker/docker/init_glue_db.sql
@@ -0,0 +1,2 @@
+GRANT ALL ON GLUE_TOOLS.* TO 'gluetools'@'%' identified by 'glue12345';
+create database GLUE_TOOLS character set UTF8; 
