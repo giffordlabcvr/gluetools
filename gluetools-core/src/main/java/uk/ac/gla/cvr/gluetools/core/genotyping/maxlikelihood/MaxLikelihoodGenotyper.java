e1e272b1d0e2e164b5307ef2068a56d58212dd62
Josh Singer <josh.singer@glasgow.ac.uk>
2019-10-15 17:05:35 +0100

    added useSingleReference option to maxLikelihoodGenotyper.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/genotyping/maxlikelihood/MaxLikelihoodGenotyper.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/genotyping/maxlikelihood/MaxLikelihoodGenotyper.java
@@ -26,10 +26,12 @@
 package uk.ac.gla.cvr.gluetools.core.genotyping.maxlikelihood;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
@@ -47,6 +49,7 @@ import uk.ac.gla.cvr.gluetools.core.genotyping.BaseGenotyper;
 import uk.ac.gla.cvr.gluetools.core.genotyping.QueryCladeCategoryResult;
 import uk.ac.gla.cvr.gluetools.core.genotyping.QueryCladeResult;
 import uk.ac.gla.cvr.gluetools.core.genotyping.QueryGenotypingResult;
+import uk.ac.gla.cvr.gluetools.core.logging.GlueLogger;
 import uk.ac.gla.cvr.gluetools.core.phylotree.PhyloBranch;
 import uk.ac.gla.cvr.gluetools.core.phylotree.PhyloLeaf;
 import uk.ac.gla.cvr.gluetools.core.phylotree.PhyloTree;
@@ -65,8 +68,12 @@ import uk.ac.gla.cvr.gluetools.core.plugins.PluginUtils;
 		description="Runs the neighbour-weighting phase of the maximum-likelihood clade assignment method")
 public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper> {
 
-	
 	private List<MaxLikelihoodCladeCategory> cladeCategories;
+
+	// If <useSingleReference> is true, only the closest reference sequence of a given clade will contribute to that clade's weight total.
+	// The default is false for backwards compatibility. However the original design does not make much sense because a 
+	// clade with a lot of references defined would be unduly heavily weighted. So a validation warning is generated in this case.
+	private boolean useSingleReference;
 	
 	public MaxLikelihoodGenotyper() {
 		super();
@@ -75,6 +82,7 @@ public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper
 		registerModulePluginCmdClass(MaxLikelihoodGenotypeFastaDocumentCommand.class);
 		registerModulePluginCmdClass(MaxLikelihoodGenotypePlacerResultCommand.class);
 		registerModulePluginCmdClass(MaxLikelihoodGenotypePlacerResultDocumentCommand.class);
+		addSimplePropertyName("useSingleReference");
 	}
 
 	@Override
@@ -82,6 +90,7 @@ public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper
 		super.configure(pluginConfigContext, configElem);
 		List<Element> categoryElems = PluginUtils.findConfigElements(configElem, "cladeCategory");
 		this.cladeCategories = PluginFactory.createPlugins(pluginConfigContext, MaxLikelihoodCladeCategory.class, categoryElems);
+		this.useSingleReference = Optional.ofNullable(PluginUtils.configureBooleanProperty(configElem, "useSingleReference", false)).orElse(false);
 	}
 
 	@Override
@@ -113,46 +122,60 @@ public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper
 				Map<String, Alignment> cladeCategoryAlmtNameToAlmt = 
 						GlueDataObject.query(cmdContext, Alignment.class, cladeCategoryAlmtSelectQuery).stream()
 						.collect(Collectors.toMap(x -> x.getName(), x -> x));
-				Map<String, Double> almtNameToScaledDistanceTotal = new LinkedHashMap<String, Double>();
-				Double allNeighboursScaledDistanceTotal = 0.0;
 				
-				Map<String, PlacementNeighbour> cladeToClosestNeighbour = new LinkedHashMap<String, PlacementNeighbour>();
+				Map<String, List<PlacementNeighbour>> cladeToClosestNeighbours = new LinkedHashMap<String, List<PlacementNeighbour>>();
 				PlacementNeighbour closestTarget = null;
-				
+
+				Map<String, Double> almtNameToScaledDistanceTotal = new LinkedHashMap<String, Double>();
+				// loop over all neighbours in order of increasing distance.
+				// 
 				for(MaxLikelihoodSinglePlacement placement: queryResult.singlePlacement) {
 					PhyloLeaf placementLeaf = MaxLikelihoodPlacer
 							.addPlacementToPhylogeny(glueProjectPhyloTree, edgeIndexToPhyloBranch, queryResult, placement);
-					List<PlacementNeighbour> neighbours = PlacementNeighbourFinder.findNeighbours(placementLeaf, new BigDecimal(distanceCutoff), null);
+					List<PlacementNeighbour> neighbours = PlacementNeighbourFinder.findNeighbours(placementLeaf, null, null);
 					for(PlacementNeighbour neighbour: neighbours) {
 						BigDecimal distance = neighbour.getDistance();
-						Double scaledDistance = Math.pow(distance.doubleValue(), distanceScalingExponent) * placement.likeWeightRatio;
-						
-						String neighbourLeafName = neighbour.getPhyloLeaf().getName();
-						List<String> neighbourAncestorAlmtNames = leafNameToAncestorAlmtNames.get(neighbourLeafName);
-						for(String neighbourAncestorAlmtName: neighbourAncestorAlmtNames) {
-							if(cladeCategoryAlmtNameToAlmt.containsKey(neighbourAncestorAlmtName)) {
-								PlacementNeighbour cladeClosestNeighbour = cladeToClosestNeighbour.get(neighbourAncestorAlmtName);
-								if(cladeClosestNeighbour == null || neighbour.getDistance().compareTo(cladeClosestNeighbour.getDistance()) < 0) {
-									cladeToClosestNeighbour.put(neighbourAncestorAlmtName, neighbour);
-								}
-								if((boolean) (neighbour.getPhyloLeaf().getUserData().get(MaxLikelihoodPlacer.PLACER_VALID_TARGET_USER_DATA_KEY))) {
-									if(closestTarget == null || neighbour.getDistance().compareTo(closestTarget.getDistance()) < 0) {
-										closestTarget = neighbour;
-									}
-								}
-								allNeighboursScaledDistanceTotal = allNeighboursScaledDistanceTotal + scaledDistance;
-								Double currentTotal = almtNameToScaledDistanceTotal.get(neighbourAncestorAlmtName);
-								if(currentTotal == null) {
-									currentTotal = 0.0;
-								}
-								currentTotal = currentTotal + scaledDistance;
-								almtNameToScaledDistanceTotal.put(neighbourAncestorAlmtName, currentTotal);
-								break;
+						// update closest target to be this neighbour as necessary.
+						if((boolean) (neighbour.getPhyloLeaf().getUserData().get(MaxLikelihoodPlacer.PLACER_VALID_TARGET_USER_DATA_KEY))) {
+							if(closestTarget == null || distance.compareTo(closestTarget.getDistance()) < 0) {
+								closestTarget = neighbour;
 							}
 						}
+						if(distance.compareTo(new BigDecimal(distanceCutoff)) <= 0) {
+							Double scaledDistance = Math.pow(distance.doubleValue(), distanceScalingExponent) * placement.likeWeightRatio;
+							String neighbourLeafName = neighbour.getPhyloLeaf().getName();
+							List<String> neighbourAncestorAlmtNames = leafNameToAncestorAlmtNames.get(neighbourLeafName);
+							for(String neighbourAncestorAlmtName: neighbourAncestorAlmtNames) {
+								if(cladeCategoryAlmtNameToAlmt.containsKey(neighbourAncestorAlmtName)) {
+									List<PlacementNeighbour> cladeClosestNeighbours = cladeToClosestNeighbours.get(neighbourAncestorAlmtName);
+									if(cladeClosestNeighbours == null) {
+										cladeClosestNeighbours = new ArrayList<PlacementNeighbour>();
+										cladeToClosestNeighbours.put(neighbourAncestorAlmtName, cladeClosestNeighbours);
+									}
+									if(cladeClosestNeighbours.size() == 0 || !useSingleReference) {
+										cladeClosestNeighbours.add(neighbour);
+										Double currentTotal = almtNameToScaledDistanceTotal.get(neighbourAncestorAlmtName);
+										if(useSingleReference) {
+											currentTotal = scaledDistance;
+										} else {
+											if(currentTotal == null) {
+												currentTotal = 0.0;
+											}
+											currentTotal = currentTotal + scaledDistance;
+										}
+										almtNameToScaledDistanceTotal.put(neighbourAncestorAlmtName, currentTotal);
+									}
+								}
+							}
+						}	
 					}
 					MaxLikelihoodPlacer.removePlacementFromPhylogeny(placementLeaf);
 				}
+				Double allNeighboursScaledDistanceTotal = 0.0;
+				for(Double scaledDistance: almtNameToScaledDistanceTotal.values()) {
+					allNeighboursScaledDistanceTotal = allNeighboursScaledDistanceTotal + scaledDistance;
+				}
+					
 				for(Map.Entry<String, Double> entry: almtNameToScaledDistanceTotal.entrySet()) {
 					String almtName = entry.getKey();
 					Double scaledDistanceTotal = entry.getValue();
@@ -168,11 +191,11 @@ public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper
 					if(queryCladeResult.percentScore >= cladeCategory.getFinalCladeCutoff()) {
 						queryCladeCategoryResult.finalClade = almtName;
 						queryCladeCategoryResult.finalCladeRenderedName = GlueDataObject.lookup(cmdContext, Alignment.class, Alignment.pkMap(almtName)).getRenderedName();
-						PlacementNeighbour closestNeighbourWithinFinalClade = cladeToClosestNeighbour.get(almtName);
-						if(closestNeighbourWithinFinalClade != null) {
+						List<PlacementNeighbour> closestNeighboursWithinFinalClade = cladeToClosestNeighbours.get(almtName);
+						if(closestNeighboursWithinFinalClade != null && closestNeighboursWithinFinalClade.size() > 0) {
 							Map<String,String> closestMemberPkMap = 
 									Project.targetPathToPkMap(ConfigurableTable.alignment_member, 
-											closestNeighbourWithinFinalClade.getPhyloLeaf().getName());
+											closestNeighboursWithinFinalClade.get(0).getPhyloLeaf().getName());
 							queryCladeCategoryResult.closestMemberAlignmentName = closestMemberPkMap.get(AlignmentMember.ALIGNMENT_NAME_PATH);
 							queryCladeCategoryResult.closestMemberSourceName = closestMemberPkMap.get(AlignmentMember.SOURCE_NAME_PATH);
 							queryCladeCategoryResult.closestMemberSequenceID = closestMemberPkMap.get(AlignmentMember.SEQUENCE_ID_PATH);
@@ -202,6 +225,14 @@ public class MaxLikelihoodGenotyper extends BaseGenotyper<MaxLikelihoodGenotyper
 	@Override
 	public List<? extends BaseCladeCategory> getCladeCategories() {
 		return cladeCategories;
+	}
+
+	@Override
+	public void validate(CommandContext cmdContext) {
+		super.validate(cmdContext);
+		if(!this.useSingleReference) {
+			GlueLogger.getGlueLogger().warning("Module '"+getModuleName()+"' of type maxLikelihoodGenotyper: consider setting <useSingleReference> to true, to avoid the bias towards clades with more references defined.");
+		}
 	}
 	
 	
