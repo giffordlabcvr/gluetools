c56b3170c88f6a7b024fc1e3519a5452a5a47567
Josh Singer <josh.singer@glasgow.ac.uk>
2019-12-13 12:45:41 +0000

    organised imports

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/blastRecogniser/BlastSequenceRecogniser.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/blastRecogniser/BlastSequenceRecogniser.java
@@ -29,7 +29,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
