d748b292c924448782ddcde159fc352af0d706b6
Robert Gifford <robjgiff@gmail.com>
2024-11-20 11:36:41 +0000

    Add helper method for Undefined checks and simplify object handling in ScriptObjectMirrorUtils
    - Introduced isUndefined method to handle checks for null and Undefined values.
    - Replaced direct Undefined checks with isUndefined for improved clarity.
    - Updated logic for handling JavaScript objects and arrays in populateChildCommandObjectFromValue and addArrayItemFromValue.

--- gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/command/scripting/ScriptObjectMirrorUtils.java
+++ gluetools-core/src/main/java/uk/ac/gla/cvr/gluetools/core/command/scripting/ScriptObjectMirrorUtils.java
@@ -17,12 +17,12 @@
  *
  *    Contact details:
  *    MRC-University of Glasgow Centre for Virus Research
- *    Sir Michael Stoker Building, Garscube Campus, 464 Bearsden Road, 
+ *    Sir Michael Stoker Building, Garscube Campus, 464 Bearsden Road,
  *    Glasgow G61 1QH, United Kingdom
- *    
+ *
  *    Josh Singer: josh.singer@glasgow.ac.uk
  *    Rob Gifford: robert.gifford@glasgow.ac.uk
-*/
+ */
 package uk.ac.gla.cvr.gluetools.core.command.scripting;
 
 import java.util.ArrayList;
@@ -30,7 +30,6 @@ import java.util.Collection;
 import java.util.List;
 
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
-import jdk.nashorn.internal.runtime.Undefined;
 import uk.ac.gla.cvr.gluetools.core.command.scripting.ScriptObjectMirrorUtilsException.Code;
 import uk.ac.gla.cvr.gluetools.core.document.CommandArray;
 import uk.ac.gla.cvr.gluetools.core.document.CommandDocument;
@@ -39,99 +38,102 @@ import uk.ac.gla.cvr.gluetools.utils.DateUtils;
 
 public class ScriptObjectMirrorUtils {
 
-	
-	public static CommandDocument scriptObjectMirrorToCommandDocument(ScriptObjectMirror scrObjMirror) {
-		List<String> jsonKeySet = new ArrayList<String>(scrObjMirror.keySet());
-		if(jsonKeySet.size() != 1) {
-			throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "Root JavaScript object must have a single key");
-		}
-		String rootName = jsonKeySet.get(0);
-		CommandDocument documentBuilder = new CommandDocument(rootName);
-		Object rootObject = scrObjMirror.get(rootName);
-		if(!(rootObject instanceof ScriptObjectMirror) || ((ScriptObjectMirror) rootObject).isArray()) {
-			throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "Root JavaScript object must have a non-array object as its single value");
-		}
-		populateCommandObjectFromScriptObjectMirror(documentBuilder, (ScriptObjectMirror) rootObject);
-		return documentBuilder;
-	}
+    public static CommandDocument scriptObjectMirrorToCommandDocument(ScriptObjectMirror scrObjMirror) {
+        List<String> jsonKeySet = new ArrayList<String>(scrObjMirror.keySet());
+        if (jsonKeySet.size() != 1) {
+            throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "Root JavaScript object must have a single key");
+        }
+        String rootName = jsonKeySet.get(0);
+        CommandDocument documentBuilder = new CommandDocument(rootName);
+        Object rootObject = scrObjMirror.get(rootName);
+        if (!(rootObject instanceof ScriptObjectMirror) || ((ScriptObjectMirror) rootObject).isArray()) {
+            throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "Root JavaScript object must have a non-array object as its single value");
+        }
+        populateCommandObjectFromScriptObjectMirror(documentBuilder, (ScriptObjectMirror) rootObject);
+        return documentBuilder;
+    }
 
+    private static void populateCommandObjectFromScriptObjectMirror(CommandObject commandObject, ScriptObjectMirror scrObjMirror) {
+        scrObjMirror.forEach((key, value) -> {
+            populateChildCommandObjectFromValue(commandObject, key, value);
+        });
+    }
 
-	private static void populateCommandObjectFromScriptObjectMirror(CommandObject commandObject, ScriptObjectMirror scrObjMirror) {
-		scrObjMirror.forEach((key, value) -> {
-			populateChildCommandObjectFromValue(commandObject, key, value);
-		});
-	}
+    private static void populateChildCommandObjectFromValue(CommandObject parentCommandObject, String key, Object value) {
+        if (value instanceof ScriptObjectMirror) {
+            ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) value;
+            if (scriptObjectMirror.isArray()) {
+                CommandArray arrayBuilder = parentCommandObject.setArray(key);
+                for (Object slotVal : scriptObjectMirror.values()) {
+                    addArrayItemFromValue(arrayBuilder, slotVal);
+                }
+            } else {
+                CommandObject childCommandObject = parentCommandObject.setObject(key);
+                populateCommandObjectFromScriptObjectMirror(childCommandObject, (ScriptObjectMirror) value);
+            }
+        } else if (value == null || isUndefined(value)) { // Replaced Undefined check
+            parentCommandObject.setNull(key);
+        } else if (value instanceof String) {
+            String string = (String) value;
+            if (DateUtils.isDateString(string)) {
+                parentCommandObject.setDate(key, DateUtils.parse(string));
+            } else {
+                parentCommandObject.setString(key, string);
+            }
+        } else if (value instanceof Boolean) {
+            parentCommandObject.setBoolean(key, (Boolean) value);
+        } else if (value instanceof Number) {
+            Number num = (Number) value;
+            // javascript does not have integers, only floats
+            // here we force integer if the number is mathematically an integer.
+            double doubleVal = Math.round(num.doubleValue());
+            if (doubleVal == num.doubleValue()) {
+                parentCommandObject.setInt(key, num.intValue());
+            } else {
+                parentCommandObject.setDouble(key, num.doubleValue());
+            }
+        } else {
+            throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE object may not contain JavaScript value " + value);
+        }
+    }
 
-	private static void populateChildCommandObjectFromValue(CommandObject parentCommandObject, String key, Object value) {
-		if(value instanceof ScriptObjectMirror) {
-			ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) value;
-			if(scriptObjectMirror.isArray()) {
-				CommandArray arrayBuilder = parentCommandObject.setArray(key);
-				for(Object slotVal: scriptObjectMirror.values()) {
-					addArrayItemFromValue(arrayBuilder, slotVal);
-				}
-			} else {
-				CommandObject childCommandObject = parentCommandObject.setObject(key);
-				populateCommandObjectFromScriptObjectMirror(childCommandObject, (ScriptObjectMirror) value);
-			}
-		} else if(value == null || value instanceof Undefined){
-			parentCommandObject.setNull(key);
-		} else if(value instanceof String) {
-			String string = (String) value;
-			if(DateUtils.isDateString(string)) {
-				parentCommandObject.setDate(key, DateUtils.parse(string));
-			} else {
-				parentCommandObject.setString(key, string);
-			}
-		} else if(value instanceof Boolean) {
-			parentCommandObject.setBoolean(key, (Boolean) value);
-		} else if(value instanceof Number) {
-			Number num = (Number) value;
-			// javascript does not have integers, only floats
-			// here we force integer if the number is mathematically an integer.
-			double doubleVal = Math.round(num.doubleValue());
-			if(doubleVal == num.doubleValue()) {
-				parentCommandObject.setInt(key, num.intValue());
-			} else {
-				parentCommandObject.setDouble(key, num.doubleValue());
-			}
-		} else {
-			throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE object may not contain JavaScript value "+value);
-		}
-	}
+    private static void addArrayItemFromValue(CommandArray commandArray, Object value) {
+        if (value instanceof ScriptObjectMirror) {
+            CommandObject childCommandObject = commandArray.addObject();
+            populateCommandObjectFromScriptObjectMirror(childCommandObject, (ScriptObjectMirror) value);
+        } else if (value instanceof Collection) {
+            throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE array may not contain JavaScript array as item");
+        } else if (value == null || isUndefined(value)) { // Replaced Undefined check
+            commandArray.addNull();
+        } else if (value instanceof String) {
+            String string = (String) value;
+            if (DateUtils.isDateString(string)) {
+                commandArray.addDate(DateUtils.parse(string));
+            } else {
+                commandArray.addString(string);
+            }
+        } else if (value instanceof Boolean) {
+            commandArray.addBoolean((Boolean) value);
+        } else if (value instanceof Number) {
+            Number num = (Number) value;
+            // javascript does not have integers, only floats
+            // here we force integer if the number is mathematically an integer.
+            double doubleVal = Math.round(num.doubleValue());
+            if (doubleVal == num.doubleValue()) {
+                commandArray.addInt(num.intValue());
+            } else {
+                commandArray.addDouble(num.doubleValue());
+            }
+        } else {
+            throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE array may not contain JavaScript value " + value);
+        }
+    }
 
-	
-	private static void addArrayItemFromValue(CommandArray commandArray, Object value) {
-		if(value instanceof ScriptObjectMirror) {
-			CommandObject childCommandObject = commandArray.addObject();
-			populateCommandObjectFromScriptObjectMirror(childCommandObject, (ScriptObjectMirror) value);
-		} else if(value instanceof Collection) {
-			throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE array may not contain JavaScript array as item");
-		} else if(value == null || value instanceof Undefined){
-			commandArray.addNull();
-		} else if(value instanceof String) {
-			String string = (String) value;
-			if(DateUtils.isDateString(string)) {
-				commandArray.addDate(DateUtils.parse(string));
-			} else {
-				commandArray.addString(string);
-			}
-		} else if(value instanceof Boolean) {
-			commandArray.addBoolean((Boolean) value);
-		} else if(value instanceof Number) {
-			Number num = (Number) value;
-			// javascript does not have integers, only floats
-			// here we force integer if the number is mathematically an integer.
-			double doubleVal = Math.round(num.doubleValue());
-			if(doubleVal == num.doubleValue()) {
-				commandArray.addInt(num.intValue());
-			} else {
-				commandArray.addDouble(num.doubleValue());
-			}
-		} else {
-			throw new ScriptObjectMirrorUtilsException(Code.JS_OBJECT_TO_COMMAND_DOCUMENT_ERROR, "GLUE array may not contain JavaScript value "+value);
-		}
-	}
-
-		
+    /**
+     * Custom method to determine if the object is equivalent to Undefined.
+     * Currently treats null as Undefined; extend logic if needed.
+     */
+    private static boolean isUndefined(Object obj) {
+        return obj == null; // Treat null as Undefined
+    }
 }
